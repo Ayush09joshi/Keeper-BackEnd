3d4b6eaa754bc612e60036ec419b75e5d80f0e44
Ayush Joshi <joshi09ayush@gmail.com>
2023-06-19 20:54:19 +0530

    main commit

---
+++ db.js
@@ -0,0 +1,16 @@
+// Database se connect hone leye
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/keeper"
+
+const connectToMongo = async () => {
+    try {
+    //   mongoose.set("strictQuery", false);
+      mongoose.connect(mongoURI);
+      console.log("Connected to Mongo Successfully!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  module.exports = connectToMongo;
